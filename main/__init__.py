cefb2656bf28c295058954378f1a818bdb773d59
devanyk2 <devanyk2@wwu.edu>
2020-06-06 17:10:57 -0700

    changing procfile and init.py

--- main/__init__.py
+++ main/__init__.py
@@ -1,9 +1,9 @@
 import os 
 from flask import Flask
 
+freeapp = Flask(__name__, instance_relative_config=True)
 def create_app(test_config=None):
-    app = Flask(__name__, instance_relative_config=True)
-
+    
     app.config.from_mapping(
             SECRET_KEY = 'yolo',
             DATABASE = os.path.join(app.instance_path, 'police.sqlite'),
