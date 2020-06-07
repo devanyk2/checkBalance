1740b623879aecde5af5349dc90182dc97f159d7
devanyk2 <devanyk2@wwu.edu>
2020-06-06 17:22:18 -0700

    changing init.py

--- main/__init__.py
+++ main/__init__.py
@@ -2,28 +2,27 @@ import os
 from flask import Flask
 
 app = Flask(__name__, instance_relative_config=True)
-def create_app(test_config=None):
     
-    app.config.from_mapping(
-            SECRET_KEY = 'yolo',
+app.config.from_mapping(
+        SECRET_KEY = 'yolo',
             DATABASE = os.path.join(app.instance_path, 'police.sqlite'),
         )
 
-    if test_config is None:
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        app.config.from_mapping(test_config)
+if test_config is None:
+    app.config.from_pyfile('config.py', silent=True)
+else:
+    app.config.from_mapping(test_config)
 
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
+try:
+    os.makedirs(app.instance_path)
+except OSError:
+    pass
 
-    from . import simdb
-    simdb.init_app(app)
+from . import simdb
+simdb.init_app(app)
 
-    from . import main
-    app.register_blueprint(main.bp)
+from . import main
+app.register_blueprint(main.bp)
 
-    return app
+return app
 
